d75314fce6225639e7ca65e435eaab0a2f2ef537
wjx002 <wjx3059180244@gmail.com>
2021-03-28 19:46:41 +0800

    学到super

--- 基础语法/src/com/oop/demo05/Person.java
+++ 基础语法/src/com/oop/demo05/Person.java
@@ -1,18 +1,30 @@
 package com.oop.demo05;
 
+import javax.naming.Name;
+
 /**
  * @Author:吴家旭
  * @date:2021/3/23 - 21:49
  */
 //人
-    //在java中，所有的类，都默认直接间接继承object
+//在java中，所有的类，都默认直接间接继承object
 public class Person {
     //public
     //protected
     //default
     //private
 
-    public void say(){
-        System.out.println("说了一句话");
+//    public void say(){
+//        System.out.println("说了一句话");
+//    }
+
+
+    protected String name = "kuangshen";
+
+    public void print() {
+        System.out.println("Person");
+    }
+    public Person(){
+        System.out.println("Person无参构造执行");
     }
 }
--- 基础语法/src/com/oop/demo05/Student.java
+++ 基础语法/src/com/oop/demo05/Student.java
@@ -7,5 +7,22 @@ package com.oop.demo05;
 //学生是人
     //子类继承父类，就会继承父类的所有方法
 public class Student extends Person{
+    private String Name="qingjiang";
 
+    public void print() {
+        System.out.println("Sutdent");
+
+    }
+
+    public void test1() {
+        print();
+        this.print();
+        super.print();
+    }
+
+    public Student() {
+        //相当于隐藏了super（）；
+        super();//调用父类构造器，必须放在子类构造器的第一行
+        System.out.println("Student无参构造执行了");
+    }
 }
